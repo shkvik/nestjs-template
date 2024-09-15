438f2d8bfce8f35e7bdc4abdba948f7b085cabb6
pendulum <rs.pendulum@gmail.com>
2024-09-15 20:12:54 +0300

    success tests and dockerfile

--- test/app.e2e.ts
+++ test/app.e2e.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { AppModule } from 'src/modules/app';
 import * as request from 'supertest';
+import { AppModule } from '../src/modules/app/app.module';
 
 
 describe('AppController (e2e)', () => {
